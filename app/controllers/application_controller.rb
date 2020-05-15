830746b60b81572df36bacfaef190c70e035cae1
Mark A Guza <guzamaguza@gmail.com>
2020-05-15 05:06:11 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,18 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :super_hero
+    end
+
+    post '/teams' do
+      @team_name = params[:team][:name]
+      @team_motto = params[:team][:motto]
+      @hero1 = Hero.new(params[:team][:members])
+      @hero2 = Hero.new(params[:team][:members])
+      @hero3 = Hero.new(params[:team][:members])
+
+      erb :team
+    end
 
 end
